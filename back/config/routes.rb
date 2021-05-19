591328bbea6008d79fddb97df7111bbafe65d655
Hiyokokeko <sukepon0914@gmail.com>
2021-05-19 14:42:54 +0900

    [fix]rubocop指摘修正

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     namespace :v1, format: 'json' do
-      mount_devise_token_auth_for 'User', at: 'auth', controllers:{
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
       resources :gears
